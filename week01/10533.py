1366c41addfe29a1c3ddcf74a4c1a8b4c6229ba1
bochain <bochainwu@gmail.com>
2021-04-02 20:46:19 +0800

    35%

--- week01/10533.py
+++ week01/10533.py
@@ -1,13 +1,48 @@
 ans = []
-n = int(input(''))
-for i in range(n):
-    temp = [int(i) input().split(' ')]
-    if ans == []:
-        ans.append(temp)
-    else:
-        for i in ans:    
-            if temp[0] < i[0] and temp[1] < i[1]:#左小右小
-                i[0] = temp[0]
-            elif temp[0] < i[1] and temp[1] > i[1]:
-                i[1] = temp[1]
-            elif
+"""
+  @@@@@
+   ######                    1
+%%%%%%                       2
+  &&                         3
+!!!!!!!!!                    4
+                $$$$         5
+"""
+while True:
+    try:
+        ans = []
+        n = int(input(''))
+        for i in range(n):
+            temp = list(map(int, input().split(' ')))
+            
+            if len(ans) == 0:
+                ans.append(temp)
+            else:
+                test = False
+                for i in ans:
+                    if temp[0] < i[0] and (temp[1] > i[0] and temp[1] <i[1]):
+                        i[0] = temp[0]
+                        test = True
+                        
+                    elif temp[1] > i[1] and (temp[0] > i[0] and temp[0] < i[1]):
+                        i[1] = temp[1]
+                        test = True
+                        
+                    elif temp[0] > i[0] and temp[1] < i[1]:
+                        test = True
+                        
+                    elif temp[0] < i[0] and i[1] < temp[1]:
+                        i[0] = temp[0]
+                        i[1] = temp[1]
+                    elif test == False:
+                        ans.append(temp)
+                        test = True
+                        
+
+        """ count """
+        answer = 0
+        for i in ans:
+            answer += (i[1] - i[0])
+
+        print(answer)
+    except Exception:
+        break
