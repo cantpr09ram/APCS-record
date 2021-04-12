b156f0844e5d658a4f5eb851a79586beb47a9b3c
bo_chain <53370227+bochainwu@users.noreply.github.com>
2021-04-12 14:26:27 +0800

    70%

--- week01/10533.py
+++ week01/10533.py
@@ -1,58 +1,13 @@
-"""
-i
-     @@@@@@@@
-i-1
-#########                  1
+import array
 
-!!!!!!!!!!!!!!!!           2
+ans = array.array('b',[0 for i in range(10000000)])
 
-     &&&&&&&&              3 ok 
-
-   &&&&&&&&&&&&            4
-$$                         5
-"""
-while True: 
-    try:
-        ans = []
-        answer = 0
-        n = int(input(''))
-        for i in range(n):
-            f,n = list(map(int, input().split(' ')))
-            ans.append([f,n])
-        ans.sort()
-        #print(ans)
-        for i in range(1, len(ans)):
-            if ans[i][0] > ans[i-1][0]:
-                if ans[i][1] < ans[i-1][1]: #2
-                    #print('2')
-                    #print(ans)
-                    ans[i][0] = ans[i-1][0]
-                    ans[i][1] = ans[i-1][1]
-                    ans[i-1] = []
-                elif ans[i-1][1] < ans[i][0]:
-                    pass
-                else:
-                    #print('1')
-                    #print(ans)
-                    ans[i][0] = ans[i-1][0]
-                    ans[i-1] = []
-
-            else: #4
-                if ans[i-1][1] > ans[i][1]:
-                    ans[i-1][1] = ans[i][1]
-                    ans[i-1] = []
-                    #print('4')
-                    #print(ans)
-                else:
-                    print('3')
-                    print(ans)
-                    pass
-
-#count
-        #print(ans)
-        for i in ans:
-            if i != []:
-                answer += (i[1] - i[0])
-        print(answer)
-    except Exception:
-        break
+times = int(input(''))
+for i in range(times):
+    start, end = map(int, input('').split(' '))
+    for i in range(start, end):
+        if ans[i] == 1:
+            pass
+        else:
+            ans[i] = 1
+print(ans.count(1))
