dda9b0e52ba056741d19ecc5ea06bb1eaa271167
bo_chain <53370227+bochainwu@users.noreply.github.com>
2021-04-26 13:34:21 +0800

    AC

--- 10510/105102.py
+++ 10510/105102.py
@@ -1,14 +1,14 @@
-n, m = map(int ,input('').split(' '))
-ans = []
+n, m = map(int, input('').split(' '))
+maxnum = []
 for i in range(n):
-    temp = list(map(int, input('').split(' ')))
-    ans.append(temp)
-a = sum([max(i) for i in ans])
-print(a)
-answer = [max(i) for i in ans]
-answer = [i for i in answer if i % a == 0]
-if len(answer) == 0:
+    a = max([int(i) for i in input('').split(' ')])
+    maxnum.append(a)
+s = sum(maxnum)
+print(s)
+div = 0
+for i in maxnum:
+    if s % i == 0:
+        div = 1
+        print(i,end = ' ')
+if  div == 0:
     print('-1')
-else:
-    for i in answer:
-        print(i, end = ' ')
