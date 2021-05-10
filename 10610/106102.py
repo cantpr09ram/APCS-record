f431c97d198a31888efa3eaf43466be83fc4654c
bo_chain <53370227+bochainwu@users.noreply.github.com>
2021-05-10 14:25:36 +0800

    50%
    8 Memory Error
    1 WA

--- 10610/106102.py
+++ 10610/106102.py
@@ -1,22 +1,44 @@
 k = int(input(''))
 string = input('')
-
-upper = 0
-lower = 0
-ans = 0
+pervious = ''
+numlist = []
+num = 0
 Max = 0
+ans = 0
 
-for i in string:
-    if i.isupper():
-        upper += 1
+if string[0].isupper():
+    pervious = 'up'
+else:
+    pervious = 'low'
+num += 1
+
+for i in range(1,len(string)):
+    if string[i].isupper() and pervious =='up':
+        num += 1
+    elif string[i].islower() and pervious =='low':
+        num += 1
     else:
-        lower += 1
-    
-    if upper < k and lower < k:
-        pass
-    elif upper == k and lower == k:
-        Max += 1
-    else:
-        ans = max(ans,Max)
-        Max = 0
-print(ans*k)
+        numlist.append(num)
+        if string[i].isupper():
+            pervious = 'up'
+
+        else:
+            pervious = 'low'
+        num = 1
+numlist.append(num)
+print(numlist)
+
+anslist = []
+for i in range(0,len(numlist)):
+    for j in range(i+1,len(numlist)+1):
+        temp = [numlist[k] for k in range(i,j)]
+        anslist.append(temp)
+
+for i in anslist:
+    if i[0] >= k and i[-1] >= k:
+        if len(i) - 2 == len([i[j] for j in range(1,len(i)-1) if i[j] == k]):
+            print(i)
+            Max = k*len(i)
+            ans = max(ans,Max)
+
+print(ans)
