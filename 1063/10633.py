57591c7977182e7c52f7d6e0f9103fda8bded233
bochain <bochainwu@gmail.com>
2021-07-23 22:42:13 +0800

    still 0%

--- 1063/10633.py
+++ 1063/10633.py
@@ -1,7 +1,7 @@
 print()
 a = int(input(''))
 dirction = int(input(''))
-dit_list = [[1,0],[-1,0],[0,-1],[0,1]]  #(up, down, left, right) = (1,3,0,2)
+dit_list = [[0,-1],[-1,0],[0,1],[1,0]]  #(left,up, down,  right) = (,0,1,2,3)
 """
 thinking
 
@@ -19,26 +19,35 @@ for i in range(a):
     arr.append(temp)
 
 numdirx, numdiry = a // 2, a // 2
-step = 0
-limit = 0 #limit of step  
-_round = 0 #if step == limit. round += 1
+step = 0 #
+limit = 1 #同方向要走幾步
+_round = 0 #通步數要轉幾次
 
-for i in range(a**2):
-    print(f'      dir {dirction} {numdirx} {numdiry}')
-    print(arr[numdirx][numdiry])
+'''
+兩個方向後同方向步數加一
+1.轉彎的時間
+2.加步數的時間
+'''
+print()
+print(arr[numdirx][numdiry])
+
+for i in range(a**2-1):
+
     step += 1
-    limit += 1
-
-    if limit == 2:#轉彎
-        _round += 1
+    if _round == 2:#加步數
+        _round = 0
         step = 0
-    else:
-        pass
-    if #e改方向
+        limit += 1 
+        dirction += 1   
+
     numdirx += dit_list[dirction][0]
     numdiry += dit_list[dirction][1]
 
+    if step == limit: #改方向
+        dirction += 1
+        _round += 1
+
     if dirction > 3:#不要過頭
         dirction = 0
-    else:
-        pass
+    print(dirction,numdirx,numdiry)
+    print(arr[numdirx][numdiry])
