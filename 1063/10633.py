968b85f48d2bca4e183e886449f75d82a100f579
bochain <bochainwu@gmail.com>
2021-07-24 23:12:28 +0800

    still 0%, but i have some ideal

--- 1063/10633.py
+++ 1063/10633.py
@@ -1,7 +1,6 @@
-print()
 a = int(input(''))
 dirction = int(input(''))
-dit_list = [[0,-1],[-1,0],[0,1],[1,0]]  #(left,up, down,  right) = (,0,1,2,3)
+dit_list = [[0,-1],[-1,0],[0,1],[1,0]]  #(left,up, down,  right) = (0,1,2,3) => (left, up, right, down) = (,0,1,3,2)
 """
 thinking
 
@@ -10,7 +9,7 @@ urdl
 rdlu
 dlur
   => dlur = lurd = urdl = rdlu
-  => 3210 = 0123 = 1230 = 2301 
+  => 2013 = 0132  = 
 """
 arr = []
 temp = []
@@ -21,33 +20,22 @@ for i in range(a):
 numdirx, numdiry = a // 2, a // 2
 step = 0 #
 limit = 1 #同方向要走幾步
-_round = 0 #通步數要轉幾次
-
+ans = ''
 '''
 兩個方向後同方向步數加一
 1.轉彎的時間
 2.加步數的時間
 '''
-print()
-print(arr[numdirx][numdiry])
-
-for i in range(a**2-1):
-
-    step += 1
-    if _round == 2:#加步數
-        _round = 0
-        step = 0
-        limit += 1 
-        dirction += 1   
-
-    numdirx += dit_list[dirction][0]
-    numdiry += dit_list[dirction][1]
-
-    if step == limit: #改方向
-        dirction += 1
-        _round += 1
-
-    if dirction > 3:#不要過頭
-        dirction = 0
-    print(dirction,numdirx,numdiry)
-    print(arr[numdirx][numdiry])
+while True:
+    for j in range(2):
+        for k in range(limit):
+            if len(arr) == 0:
+                break
+            else:
+                x = arr[numdirx][numdiry]
+                arr.remove(0)
+                print(x)
+                numdirx += dit_list[dirction][0]
+                numdiry += dit_list[dirction][1]
+        dirction = (dirction + 1) % 4
+    limit += 1
