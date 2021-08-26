e876c4f18b4b14db85eb01d57776ef166ab8de2e
bochain <bochainwu@gmail.com>
2021-08-26 11:06:41 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-a = [[1 for j in range(5)] for i in range(6)]
+'''
+a = [[1 for j in range(5)], 
+     [2 for j in range(5)],
+     [3 for j in range(5)],
+     [4 for j in range(5)],
+     [5 for j in range(5)],
+     [6 for j in range(5)]]
 for i in range(6):
     for j in range(5):
         print(a[i][j], end='')
@@ -8,14 +14,32 @@ print()
 
 x, y = 0, 0
 while True:
-    if y == 4:
+    if x == 6:
         print() 
-        y = 0
-        x += 1
+        x = 0
+        y += 1
 
-    print(a[y][x] ,end='')
+    print(a[x][y] ,end='')
 
-    y += 1
-
-    if x == 4 and y == 5:
+    x += 1
+    if x==6 and y==4:
         break
+    '''
+'''
+a = 1
+a = b
+print(b)
+for i in range(5):
+    if a == 1:
+        print('1')
+        pass
+    elif a == 1:
+        print('2')
+    else:
+        print('x')
+        '''
+a, b,c,d = 0, 0 ,0 ,1
+
+for i in range(4):
+    c, d, a, b = b, c, d, a
+    print(a,b,c,d)
